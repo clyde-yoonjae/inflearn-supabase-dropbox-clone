fc2e2977ba9600772d8299ea5d684da428b10e38
yoooonbong <a1056719@gmail.com>
2024-12-02 01:29:37 +0900

    ✨ 등록기능 구현

--- components/dropbox-image.tsx
+++ components/dropbox-image.tsx
@@ -1,20 +1,24 @@
 'use client';
 
 import { IconButton } from '@material-tailwind/react';
+import { getImageUrl } from 'utils/supabase/storage';
 
-export default function DropboxImage() {
+export default function DropboxImage({ image }) {
   return (
     <div className='relative w-full flex flex-col gap-2 p-4 border border-gray-100 rounded-2xl shadow-md'>
+      {/* Image */}
       <div>
         <img
-          src='/images/dropbox_icon.png'
-          alt='h'
+          src={getImageUrl(image.name)}
           className='w-full aspect-square rounded-2xl'
         />
       </div>
-      <div>cutedog.jpeg</div>
+
+      {/* FileName */}
+      <div className=''>{image.name}</div>
+
       <div className='absolute top-4 right-4'>
-        <IconButton color='red' {...({ placeholder: '' } as any)}>
+        <IconButton onClick={() => {}} color='red'>
           <i className='fas fa-trash' />
         </IconButton>
       </div>
--- components/file-dragdropzone.tsx
+++ components/file-dragdropzone.tsx
@@ -1,10 +1,50 @@
 'use client';
 
+import { Button } from '@material-tailwind/react';
+import { useMutation } from '@tanstack/react-query';
+import { uploadFile } from 'actions/storageActions';
+import { queryClient } from 'config/ReactQueryClientProvider';
+import { useRef } from 'react';
+
 export default function FileDragDropZone() {
+  const fileRef = useRef(null);
+  const uploadImageMutation = useMutation({
+    mutationFn: uploadFile,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['images'],
+      });
+    },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const file = fileRef.current.files?.[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      uploadImageMutation.mutate(formData, {
+        onSuccess: (result) => {
+          console.log(result);
+        },
+        onError: (error) => {
+          console.error('Upload failed', error);
+        },
+      });
+    }
+  };
+
   return (
-    <section className='w-full py-20 border-2 border-dotted border-indigo-700 flex flex-col items-center justify-center'>
-      <input type='file' className='' />
-      <p>파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요</p>
-    </section>
+    <form
+      onSubmit={handleSubmit}
+      className='w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center'
+    >
+      <input ref={fileRef} type='file' className='' />
+      <p>파일을 여기에 끌어다 놓거나 클릭하여 업로드하세요.</p>
+      <Button loading={uploadImageMutation.isPending} type='submit'>
+        파일 업로드
+      </Button>
+    </form>
   );
 }
